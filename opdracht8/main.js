953367a80d41a0df8d7536e04fe0d5b5f4c2eb47
jaymiel <65394374+jaymiel@users.noreply.github.com>
2021-03-01 15:38:37 +0100

    Add files via upload

---
+++ opdracht8/main.js
@@ -0,0 +1,3 @@
+const x = fetch('https://backendcities.herokuapp.com/?cuid=hajIUIksk983LLP11112220&start=1&size=30').then(r => r.json())
+document.getElementById("div").innerHTML;
+document.appendChild('')
