71d53d12291008f0c4ee94577a51bb464486c957
Stijn <Stijn@MacBook-Pro-van-Stijn.local>
2021-03-08 15:16:26 +0100

    nieuwe main.js

--- opdracht8/main.js
+++ opdracht8/main.js
@@ -1,3 +1,13 @@
-const x = fetch('https://backendcities.herokuapp.com/?cuid=hajIUIksk983LLP11112220&start=1&size=30').then(r => r.json())
-document.getElementById("div").innerHTML;
-document.appendChild('')
+const x = fetch('https://backendcities.herokuapp.com/?cuid=hajIUIksk983LLP11112220&start=1&size=30')
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+        for (const test of data){
+            console.log(test.name);
+            const a = document.createElement("div");
+            a.innerHTML = test.name;
+            document.body.appendChild(a);
+        }
+    });
+
+
