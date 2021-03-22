ebbd60d2f712200aaba0a69f57af8065081d04c3
jaymiel <jaymiel.polonius@ictmbo.nl>
2021-03-22 14:21:20 +0100

    volgende

--- src/index.js
+++ src/index.js
@@ -2,28 +2,29 @@ const queryString = require('query-string');
 
 const parsed = queryString.parse(location.search);
 
-console.log(parsed.pagina);
 let p = parsed.pagina;
-const startpoint = ((p - 1) * 30) + 1;
-const  x = fetch(`https://backendcities.herokuapp.com/?cuid=hajIUIksk983LLP11112220&start=${startpoint}&size=30`)
+const startPoint = ((p - 1) * 30) + 1;
+const  x = fetch(`https://backendcities.herokuapp.com/?cuid=hajIUIksk983LLP11112220&start=${startPoint}&size=30`)
     .then(response => response.json())
     .then(data => {
-        console.log(data)
         for (const test of data){
-            console.log(test.name);
             const a = document.createElement("div");
             a.innerHTML = test.name;
-            document.body.appendChild(a);
+            document.getElementById('div').appendChild(a);
         }
     });
 
 
 
-const a = document.createElement('a');
-const node = document.createTextNode("Vorige");
-a.appendChild(node);
-const e = document.getElementById("div");
-a.href = `?p=${p++}`;
+const next = document.createElement('a');
+next.href = `index.html?p=${p+1}`;
+next.innerText = "Volgende";
+
+const page = document.getElementById("pagina");
+console.log(page);
+page.appendChild(next);
+
+
 
 // const btn = document.getElementById('btn').addEventListener('click', function () {
 //    // for (p = 1; p )
